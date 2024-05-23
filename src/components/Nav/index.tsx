fd9d83cbdea6131b677f05a2cb3251f5f4d79bd7
zewei.zhang <zewei@57blocks.com>
2024-05-23 09:29:28 +0800

    feat: add logo

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -3,11 +3,14 @@
 import Link from "next/link";
 import WalletConnector from "../WalletConnector";
 import Search from "../Search";
+import Logo from "./logo";
 
 export default function Nav() {
     return <nav className="h-[70px] flex justify-between items-center px-8">
         <h1 className="text-2xl w-[150px] font-bold text-indigo-600">
-            <Link href='/'>Dolphin</Link>
+            <Link href='/'>
+                <Logo />
+            </Link>
         </h1>
         <Search />
         <div>
